2b76ec5a3fdfb401b786d56285c9322ec0d605d6
Megumi fuji <knz1209sin@yahoo.co.jp>
2020-10-28 06:10:01 +0900

    内容変更

--- 10allquestion/script.js
+++ 10allquestion/script.js
@@ -9,6 +9,16 @@ function changeColor (){
     q6_text.style.color="blue";
 }
 
+// Q 6 変数使わない場合
+// function changeColor (){
+// 	document.querySelector("#q6-text").style.color="blue";
+// }
+
+// Q 6 イベントリスナー使う場合
+// htmlのonclickはいらない
+// document.querySelector("#q6-text").addEventListener("click",function(){
+// this.style.color="blue";
+// }
 
 // Q 7
 
@@ -31,29 +41,34 @@ message.addEventListener("mouseover",function(){
 
 
 // Q 9
- var input = document.querySelector("#q9-input");
- var text = document.querySelector("#q9-result");
- var btn = document.querySelector('#q9-btn');
+var btn = document.querySelector('#q9-btn');
 console.log(btn);
 btn.addEventListener('click', function () {
+var text = document.querySelector("#q9-result");
+var input = document.querySelector("#q9-input");
   text.textContent = input.value;
 })
 
 // Q 10
- var input2 = document.querySelector("#q10-input");
- var text2 = document.querySelector("#q10-result");
+// 問題６０歳以上を入力された場合「６０歳以上の人は半額です。それ以外の人はふつうの数字を入力
  var btn2 = document.querySelector('.q10-btn');
 console.log(btn2);
 btn2.addEventListener('click', function () {
-	if (input2.value >= 20) {
-    console.log("利用できます")
-    text2.textContent = "利用できます";
-  	}else{
-	console.log("利用できません");
-	text2.textContent = "20歳未満の方の利用は禁止です。";
+ var input2	 = document.querySelector("#q10-input");
+ var text2 = document.querySelector("#q10-result");
+	if (input2.value < 20) {
+   text2.textContent = "20歳未満の方の利用は禁止です。";
+  	}else if(input2.value >= 60){
+	text2.textContent = "60歳以上の方は半額です。";
+	}else{
+	text2.textContent = input2.value;
 	}
-  // text2.textContent = input2.value;
-})
+});
+
+
+
+
+
 
 
 
